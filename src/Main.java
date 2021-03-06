e0335e497d749ea965951195dcabbb32b1920468
Robert Dolgopolov <siidousdart@gmail.com>
2021-03-07 01:58:59 +0500

    alphabet & code

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+//        for (char c = 'a'; c<= 'z'; c++);
+//        {
+//            alphabet[c - 'a'] =c;
+//        }
+        for(char i='a'; i<='z'; i++)
+        {
+            int c=i;
+            System.out.println(i+" :"+c+".");
+        }
+        System.out.println("\n ========================\n");
+        for(char j='а'; j<='я'; j++)
+        {
+            int x=j;
+            System.out.println(j+" :"+x+".");
+        }
+    }
+}
